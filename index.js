3609bf371c112faa30d291aa90a2263874e93dd6
MohammedShabbar Manek <shabbar_manek@live.com>
2018-02-08 17:46:07 +0000

    adds docblock to postToGroup method

--- index.js
+++ index.js
@@ -11,17 +11,25 @@ const FB = require('fb');
       FB.setAccessToken(accessToken);
     };
 
+    /*
+    *  Posts something to a group as a bot user. To do this, your API token must
+    *  have the "Manage group content" permission.
+    *
+    *  @param groupID: The fbid of the group that you would like to post to.
+    *                  Typically, you can find it by going to the group on
+    *                  Workplace and looking at the url - groups/<groupID>
+    *  @param data: Data is an object that contains information on what is to be
+    *               posted to workplace.
+    *  @param data.url: The url of the photo that is to be posted
+    *  @param data.message: The body of the post to the group in a string.
+    */
     const postToGroup = async function postToGroup(groupID, data) {
       return new Promise((resolve, reject) => {
-        const path = `${groupID}/feed`;
-        FB.api(path, 'post', {
-          message: data.message,
-        }, (res) => {
-          if (res.error) {
-            reject(res.error);
-          } else {
-            resolve(res);
-          }
+        const path =
+          data.hasOwnProperty('url') ? `${groupID}/photos` : `${groupID}/feed`;
+
+        FB.api(path, 'post', data, (res) => {
+          res.hasOwnProperty('error') ? reject(res.error) : resolve(res);
         });
       });
     };
