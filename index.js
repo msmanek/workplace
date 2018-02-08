dd4cc7393ece82ce6429fa920018dcb84c595eb5
MohammedShabbar Manek <shabbar_manek@live.com>
2018-02-08 14:59:17 +0000

    adds prelim post to group method

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
-exports.printMsg = function() {
-  console.log("hello, workplace!");
-}
+(() => {
+  var WP = (() => {
+    const FB = require('fb');
+
+    /*
+    *  Initialize the workplace API as a wrapper on the request package
+    *
+    *  @param access_token: the bearer auth token for the API
+    */
+    var setAccessToken = (access_token) => {
+      FB.setAccessToken(access_token);
+    }
+
+    var postToGroup = (group_id, data, callback) => {
+      callback = callback || () => {};
+      FB.api(group_id + '/feed', 'post', {
+        message: data.message,
+      }, callback);
+    }
+
+    return {
+      setAccessToken: setAccessToken,
+      postToGroup: postToGroup,
+    }){};
+  };
+  module.exports = WP;
+})();
